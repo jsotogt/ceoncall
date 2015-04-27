5fa431ab7fb7a562c85de9b4faa9ec9ab0c2f73f
Jaime Soto <jsoto@accesso.com>
2015-04-27 13:33:26 -0400

    removed default data from main application

--- src/main/java/ceoncall/Application.java
+++ src/main/java/ceoncall/Application.java
@@ -11,30 +11,10 @@ import ceoncall.domain.TeamMember;
 
 @SpringBootApplication
 public class Application {
-	
-  	@PersistenceContext
-  	EntityManager em;
 
     public static void main(String[] args) 
     {
         SpringApplication.run(Application.class, args);
     }
-    
-    public void addDefaultTeamMembers()
-    {
-      String jpql = "SELECT t FROM TeamMember t";
-  		
-  		TypedQuery<TeamMember> query = em.createQuery(jpql, TeamMember.class);
-  		
-  		if (!query.getResultList().isEmpty())
-  			return;
-  		
-  		TeamMember t1 = new TeamMember("Matt Damon", "123456", "tehdamon@accesso.com");
-  		TeamMember t2 = new TeamMember("Matt Damon2", "987654", "tehdamon2@accesso.com");
-  		TeamMember t3 = new TeamMember("Matt Damon3", "645789", "tehdamon3@accesso.com");
 
-  		em.persist(t1);
-  		em.persist(t2);
-  		em.persist(t3);
-    }
 }
