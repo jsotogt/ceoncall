5eeac66331dbf00627486b814427559d519f67b6
Jaime Soto <jsoto@accesso.com>
2015-05-18 18:04:44 -0400

    added jackson config

---
+++ src/main/java/ceoncall/Jackson2Config.java
@@ -0,0 +1,22 @@
+package ceoncall;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@EnableTransactionManagement
+public class Jackson2Config {
+
+    @Bean
+    public MappingJackson2HttpMessageConverter mappingJackson2HttpMessageConverter() {
+        MappingJackson2HttpMessageConverter jsonConverter = new MappingJackson2HttpMessageConverter();
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+        jsonConverter.setObjectMapper(objectMapper);
+        return jsonConverter;
+    }
+}
