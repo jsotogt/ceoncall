8c6042edbe5f60935ff3a65a73fef8458ef8531a
Jaime Soto <jsoto@accesso.com>
2015-05-21 17:27:44 -0400

    updated team controller

--- src/main/java/ceoncall/web/TeamController.java
+++ src/main/java/ceoncall/web/TeamController.java
@@ -3,6 +3,8 @@ package ceoncall.web;
 import ceoncall.domain.Team;
 import ceoncall.service.TeamService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -19,43 +21,44 @@ public class TeamController {
         return teamService.findAll();
     }
 
-    @RequestMapping(value="/team", method= RequestMethod.POST)
-    public Team create(@RequestBody Team t)
-    {
-        teamService.save(t);
-
-        return t;
-    }
-
-    @RequestMapping(value="/team/{id}", method=RequestMethod.PUT)
-    public Team update(@PathVariable("id") int id, @RequestBody Team t)
-    {
-        Team told = teamService.findById(id);
-
-        if (told == null)
-            return told;
-
-        t.setId(id);
-
-        return teamService.update(t);
-    }
-
     @RequestMapping(value="/team/{id}", method=RequestMethod.GET)
     public Team get(@PathVariable("id") int id)
     {
         return teamService.findById(id);
     }
 
+    @RequestMapping(value="/team", method= RequestMethod.POST)
+    public ResponseEntity<String> create(@RequestBody Team t)
+    {
+        teamService.save(t);
+
+        return new ResponseEntity<String>(HttpStatus.OK);
+    }
+
+    @RequestMapping(value="/team/{id}", method=RequestMethod.PUT)
+    public ResponseEntity<String> update(@PathVariable("id") int id, @RequestBody Team t)
+    {
+        Team told = teamService.findById(id);
+
+        if (told == null)
+            return new ResponseEntity<String>(HttpStatus.NOT_FOUND);
+
+        t.setId(id);
+        teamService.update(t);
+
+        return new ResponseEntity<String>(HttpStatus.OK);
+    }
+
     @RequestMapping(value="/team/{id}", method=RequestMethod.DELETE)
-    public Team delete(@PathVariable("id") int id)
+    public ResponseEntity<String> delete(@PathVariable("id") int id)
     {
         Team t = teamService.findById(id);
 
-        if (t == null) return null;
+        if (t == null) return new ResponseEntity<String>(HttpStatus.NOT_FOUND);
 
         teamService.delete(t);
 
-        return t;
+        return new ResponseEntity<String>(HttpStatus.OK);
     }
     
 }
