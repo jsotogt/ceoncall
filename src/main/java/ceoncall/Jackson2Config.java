83b31605f4dec1da536c65bdf2448fc4884e046a
Jaime Soto <jsoto@accesso.com>
2015-05-27 08:05:01 -0400

    addressed unknown json properties

--- src/main/java/ceoncall/Jackson2Config.java
+++ src/main/java/ceoncall/Jackson2Config.java
@@ -1,6 +1,7 @@
 package ceoncall;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -21,6 +22,7 @@ public class Jackson2Config {
 
         ObjectMapper objectMapper = new ObjectMapper();
         objectMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
         objectMapper.setDateFormat(df);
 
         return objectMapper;
